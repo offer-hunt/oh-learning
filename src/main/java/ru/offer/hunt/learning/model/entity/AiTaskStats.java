b30e3f8bf0383484ba018505e4b4d0313e2b28a2
termoler <misaib@yandex.ru>
2025-12-02 21:01:44 +0700

    fix import

--- src/main/java/ru/offer/hunt/learning/model/entity/AiTaskStats.java
+++ src/main/java/ru/offer/hunt/learning/model/entity/AiTaskStats.java
@@ -1,11 +1,10 @@
 package ru.offer.hunt.learning.model.entity;
 
-import java.time.OffsetDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -18,8 +17,7 @@ import ru.offer.hunt.learning.model.id.AiTaskStatsId;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class AiTaskStats {
 
-  @EmbeddedId
-  private AiTaskStatsId id;
+  @EmbeddedId private AiTaskStatsId id;
 
   @Column(name = "hints_used")
   private Integer hintsUsed;
