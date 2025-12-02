c16be68c2c193c1976aa16f29abef5d05f73a66a
termoler <misaib@yandex.ru>
2025-12-02 21:18:59 +0700

    fix import

--- src/main/java/ru/offer/hunt/learning/controller/QuestionStateController.java
+++ src/main/java/ru/offer/hunt/learning/controller/QuestionStateController.java
@@ -1,6 +1,7 @@
 package ru.offer.hunt.learning.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
@@ -8,7 +9,15 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.offer.hunt.learning.model.dto.QuestionStateDto;
 import ru.offer.hunt.learning.model.dto.QuestionStateUpsertRequest;
 import ru.offer.hunt.learning.service.QuestionStateService;
--- src/main/java/ru/offer/hunt/learning/model/id/AiTaskStatsId.java
+++ src/main/java/ru/offer/hunt/learning/model/id/AiTaskStatsId.java
@@ -3,7 +3,12 @@ package ru.offer.hunt.learning.model.id;
 import jakarta.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.UUID;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
 @Getter
